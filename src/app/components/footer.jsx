2223a41157c9e5ea29ed608081dabf09fb184960
Raeid <raeidus15@gmail.com>
2024-05-16 14:19:13 -0400

    update
    added animations

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdAlternateEmail } from "react-icons/md";
 import { RxDiscordLogo } from "react-icons/rx";
 import { FaInstagram } from "react-icons/fa";
 import { DiGithubBadge } from "react-icons/di";
 import { FaLinkedin } from "react-icons/fa6";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 function Footer() {
     const handleDiscordClick = () => {
@@ -13,32 +15,53 @@ function Footer() {
         window.alert(`Discord tag "${discordTag}" copied to clipboard!`);
     };
 
+    const [leftRef, leftInView] = useInView();
+    const [rightRef, rightInView] = useInView();
+
     return (
         <div id="contact" className="bg-gray-200 py-12">
             <div className="container mx-auto px-6 md:px-8 lg:px-12 xl:px-20">
                 <h1 className="text-blue-600 text-3xl font-bold text-center mb-8">Get in Touch with Me!</h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-12">
-                    <div className="p-6 bg-white rounded-md shadow-md">
-                        <h2 className="text-2xl text-blue-900 font-bold mb-4">Got any questions?</h2>
-                        <p className="text-lg text-blue-600 mb-6">Feel free to reach out. Here are some of My Socials:</p>
-                        <div className="flex items-center space-x-6">
-                            <a href="https://github.com/Raeid-U"><DiGithubBadge className="w-12 h-12 text-blue-600 hover:text-blue-900" /></a>
-                            <a href="https://www.linkedin.com/in/raeid-usmanali/"><FaLinkedin className="w-12 h-12 text-blue-600 hover:text-blue-900" /></a>
-                            <button onClick={handleDiscordClick}><RxDiscordLogo className="w-12 h-12 text-blue-600 hover:text-blue-900 cursor-pointer" /></button>
-                            <a href="https://www.instagram.com/raeid2006/"><FaInstagram className="w-12 h-12 hover:text-blue-900 text-blue-600" /></a>
-                        </div>
+                    <div className="p-6 bg-gray-200 rounded-md shadow-md" ref={leftRef}>
+                        <motion.div
+                            className="bg-white rounded-md"
+                            initial={{ x: -100, opacity: 0 }}
+                            animate={leftInView ? { x: 0, opacity: 1 } : { x: -100, opacity: 0 }}
+                            transition={{ type: "spring", duration: 1 }}
+                        >
+                            <div className="p-6">
+                                <h2 className="text-2xl text-blue-900 font-bold mb-4">Got any questions?</h2>
+                                <p className="text-lg text-blue-600 mb-6">Feel free to reach out. Here are some of My Socials:</p>
+                                <div className="flex items-center space-x-6">
+                                    <a href="https://github.com/Raeid-U"><DiGithubBadge className="w-12 h-12 text-blue-600 hover:text-blue-900" /></a>
+                                    <a href="https://www.linkedin.com/in/raeid-usmanali/"><FaLinkedin className="w-12 h-12 text-blue-600 hover:text-blue-900" /></a>
+                                    <button onClick={handleDiscordClick}><RxDiscordLogo className="w-12 h-12 text-blue-600 hover:text-blue-900 cursor-pointer" /></button>
+                                    <a href="https://www.instagram.com/raeid2006/"><FaInstagram className="w-12 h-12 hover:text-blue-900 text-blue-600" /></a>
+                                </div>
+                            </div>
+                        </motion.div>
                     </div>
-                    <div className="p-6 bg-white rounded-md shadow-md">
-                        <div className="flex flex-col space-y-4">
-                            <button className="text-xl text-blue-600 bg-grey-200 rounded-full p-3 flex items-center justify-start hover:bg-gray-300 transition duration-300">
-                                <FaPhoneAlt className="w-10 h-10 bg-gray-200 rounded-full mr-4 p-2" />
-                                <span className="truncate">+1 (647)-572-4036</span>
-                            </button>
-                            <button className="text-xl text-blue-600 bg-grey-200 rounded-full p-3 flex items-center justify-start hover:bg-gray-300 transition duration-300">
-                                <MdAlternateEmail className="w-10 h-10 bg-gray-200 rounded-full mr-4 p-2" />
-                                <span className="truncate">raeidus15@gmail.com</span>
-                            </button>
-                        </div>
+                    <div className="p-6 bg-gray-200 rounded-md shadow-md" ref={rightRef}>
+                        <motion.div
+                            className="bg-white rounded-md"
+                            initial={{ x: 100, opacity: 0 }}
+                            animate={rightInView ? { x: 0, opacity: 1 } : { x: 100, opacity: 0 }}
+                            transition={{ type: "spring", duration: 1 }}
+                        >
+                            <div className="p-6">
+                                <div className="flex flex-col space-y-4">
+                                    <button className="text-xl text-blue-600 bg-gray-100 rounded-full p-3 flex items-center justify-start hover:bg-gray-300 transition duration-300">
+                                        <FaPhoneAlt className="w-10 h-10 bg-gray-200 rounded-full mr-4 p-2" />
+                                        <span className="truncate">+1 (647)-572-4036</span>
+                                    </button>
+                                    <button className="text-xl text-blue-600 bg-gray-100 rounded-full p-3 flex items-center justify-start hover:bg-gray-300 transition duration-300">
+                                        <MdAlternateEmail className="w-10 h-10 bg-gray-200 rounded-full mr-4 p-2" />
+                                        <span className="truncate">raeidus15@gmail.com</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </motion.div>
                     </div>
                 </div>
             </div>
--- src/app/components/hero.jsx
+++ src/app/components/hero.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Background from './background';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const LandingHero = ({ scrollToSection }) => {
-    return (
-      <Background>
-        <section id="home">
+  const [ref, inView] = useInView();
+  const containerRef = useRef();
+
+  return (
+    <Background>
+      <section id="home">
         <div className="max-w-7xl mx-auto px-4 py-32 flex flex-col md:flex-row items-center z-8 relative">
-          <div className="md:w-1/2 mb-8 md:mb-0 md:text-right md:pr-9 border-r-2 border-white pr-8">
-            <img src={"./assets/heroimg1.jpg"} alt={"alt"} className="w-full rounded-md border-8 md:w-3/4 lg:w-3/4 xl:w-3/3 float-right" />
+          <div className="md:w-1/2 mb-8 md:mb-0 md:text-right md:pr-9 border-r-2 border-white pr-8" ref={containerRef}>  
+              <img src={"./assets/heroimg1.jpg"} alt={"alt"} className="w-full rounded-md border-8 md:w-3/4 lg:w-3/4 xl:w-3/3 float-right" />
           </div>
           <div className="md:w-1/2 md:pl-8 text-white">
             <h1 className="text-5xl font-bold mb-4">Hi!</h1>
@@ -21,12 +26,19 @@ const LandingHero = ({ scrollToSection }) => {
             <p className="text-lg mb-6">
               Check out some of my projects below or, if you would like to talk, hit the button to get into contact with me!
             </p>
+            <motion.div 
+              ref={ref}
+              initial={{ x: 100, opacity: 0 }} 
+              animate={inView ? { x: 0, opacity: 1 } : { x: 100, opacity: 0 }}
+              transition={{ type: 'spring', duration: 1 }}
+            >
             <button className="bg-white text-blue-900 py-2 px-6 rounded-lg hover:bg-blue-100 transition duration-300" onClick={() => scrollToSection("contact")}>Get in Contact</button>
+            </motion.div>
           </div>
         </div>
-        </section>
-      </Background>
-    );
+      </section>
+    </Background>
+  );
 };
 
 export default LandingHero;
