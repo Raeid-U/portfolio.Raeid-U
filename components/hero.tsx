5293a7c303cae27b8a75d2954c6729c661b0ddac
Raeid-U <raeidus15@gmail.com>
2025-02-02 21:07:38 -0500

    mobileview: hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,24 +4,22 @@ import Reimagined from "./reimagined";
 
 export default function Hero() {
   return (
-    <section className="flex w-full h-screen">
-      <div className="w-1/2 h-full bg-foreground flex items-center justify-center text-offwhite">
-        <div className="w-3/4 flex flex-col items-center text-center mt-16">
+    <div className="flex flex-col md:flex-row w-full min-h-screen">
+      <div className="flex flex-col justify-center items-center bg-foreground text-offwhite p-8 w-full md:w-1/2">
+        <div className="w-5/6 lg:w-3/4 flex flex-col items-center text-center space-y-6">
           <Reimagined />
-
           <HeroBox />
         </div>
       </div>
 
-      <div className="w-1/2 h-full relative">
+      <div className="relative w-full md:w-1/2 h-[50vh] md:h-auto">
         <Image
           src="/hero_sat.jpg"
           alt="Hero Image"
-          layout="fill"
-          objectFit="cover"
-          className="w-full h-full"
+          fill
+          className="object-cover w-full h-full"
         />
       </div>
-    </section>
+    </div>
   );
 }
--- components/hero_box.tsx
+++ components/hero_box.tsx
@@ -9,18 +9,18 @@ export default function HeroBox() {
       </div>
 
       <div className="border p-8 font-jetbrains">
-        <h1 className="text-2xl tracking-wide font-semibold mt-2">
+        <h1 className="text-lg md:text-xl tracking-wide font-semibold mt-2">
           I’m <span className="text-highlight">Raeid</span>
         </h1>
 
-        <p className="mt-4 text-md leading-relaxed">
+        <p className="mt-4 text-sm md:text-md leading-relaxed">
           full stack dev, travel, cloud computing, finance, chirography,
           automation, drumming, photography
         </p>
 
         <p className="mt-8 text-sm">i’ve got a lot of interests</p>
 
-        <button className="mt-6 px-4 py-2 text-offwhite hover:tracking-widest transition-all duration-700">
+        <button className="mt-6 px-4 py-2 text-xs md:text-sm text-offwhite hover:tracking-widest transition-all duration-700">
           work with me →
         </button>
       </div>
